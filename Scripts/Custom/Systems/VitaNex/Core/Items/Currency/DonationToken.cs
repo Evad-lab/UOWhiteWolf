afb4d2e081948e7496e18f9e3ca6b6b19cddcf86
Evad <25418791+Evad-lab@users.noreply.github.com>
2022-03-29 22:13:09 -0400

    Revert "Delete Scripts/Custom/Systems/VitaNex directory"
    This reverts commit 797cc4ee972a31b198ba668ca76a745d0f3553d8.

---
+++ Scripts/Custom/Systems/VitaNex/Core/Items/Currency/DonationToken.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using VitaNex.Items;
+using VitaNex.Mobiles;
+
+namespace Server.Items
+{
+    public class DonationToken : VendorToken
+    {
+
+        [Constructable]
+        public DonationToken() : this( 1 )
+        {
+        }
+        [Constructable]
+        public DonationToken( int amountFrom, int amountTo ) : this( Utility.RandomMinMax( amountFrom, amountTo ) )
+        {
+        }
+        [Constructable]
+        public DonationToken( int amount ) : base( 0xEED )
+        {
+            Name = "Donation Token";
+			Amount = amount;
+			Hue = 115;
+        }
+        public DonationToken( Serial serial ) : base( serial )
+        {
+        }
+
+
+        
+
+        public override void Serialize( GenericWriter writer )
+        {
+            base.Serialize( writer );
+
+            writer.Write( (int) 0 ); // version
+        }
+
+        public override void Deserialize( GenericReader reader )
+        {
+            base.Deserialize( reader );
+
+            int version = reader.ReadInt();
+        }
+    }
+}
